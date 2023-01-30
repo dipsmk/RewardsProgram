3218a7be4674f9694fad1cfa01df881dafef8b2e
dipsmk <38143441+dipsmk@users.noreply.github.com>
2023-01-29 21:00:09 -0600

    Add files via upload

---
+++ rewardPointApplication/src/test/java/com/demo/rewardprogram/model/MonthlyRewardsTest.java
@@ -0,0 +1,36 @@
+package com.demo.rewardprogram.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.demo.rewardprogram.model.CustomerDTO;
+import com.demo.rewardprogram.model.MonthlyRewards;
+import com.demo.rewardprogram.model.TransactionRecordDTO;
+import com.demo.rewardprogram.util.DataBuilder;
+
+public class MonthlyRewardsTest {
+	private DataBuilder dataBuilder = new DataBuilder();
+
+	private CustomerDTO mary = dataBuilder.createCustomerDTO("Mary");
+
+	private MonthlyRewards testClass = new MonthlyRewards(mary);
+
+	@Test
+	void test_getMonthlyTotalRewardPoint() {
+
+		List<TransactionRecordDTO> trans = new ArrayList<>();
+		trans.add(dataBuilder.createTransactionRecordDTO(mary, "90", 2, 1));
+		trans.add(dataBuilder.createTransactionRecordDTO(mary, "50", 2, 1));
+		testClass.getMonthlyTrans().put(Month.JUNE, trans);
+
+		assertEquals(0, testClass.getMonthlyTotalRewardPoint().get(Month.JUNE).compareTo(new BigDecimal("40")));
+
+	}
+
+}
